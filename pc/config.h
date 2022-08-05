d59ea9ea50b1daf21b246494ffbc5ab052abd539
Arnold D. Robbins <arnold@skeeve.com>
2022-08-05 13:26:42 +0300

    Next test tarball.

--- pc/config.h
+++ pc/config.h
@@ -373,7 +373,7 @@
 #define PACKAGE_NAME "GNU Awk"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "GNU Awk 5.1.62"
+#define PACKAGE_STRING "GNU Awk 5.1.63"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "gawk"
@@ -382,7 +382,7 @@
 #define PACKAGE_URL "http://www.gnu.org/software/gawk/"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "5.1.62"
+#define PACKAGE_VERSION "5.1.63"
 
 /* Define to 1 if *printf supports %a format */
 #define PRINTF_HAS_A_FORMAT 1
@@ -510,7 +510,7 @@
 
 
 /* Version number of package */
-#define VERSION "5.1.62"
+#define VERSION "5.1.63"
 
 /* Number of bits in a file offset, on hosts where this is settable. */
 #undef _FILE_OFFSET_BITS
