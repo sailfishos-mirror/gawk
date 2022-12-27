a5010fa20ae29366d290e2becfa212602e3f69be
Arnold D. Robbins <arnold@skeeve.com>
2022-12-27 23:15:26 +0200

    Finish libsigsegv cleanup.

--- main.c
+++ main.c
@@ -211,7 +211,6 @@ int
 main(int argc, char **argv)
 {
 	int i;
-	char *extra_stack;
 	bool have_srcfile = false;
 	SRCFILE *s;
 	char *cp;
@@ -554,10 +553,6 @@ main(int argc, char **argv)
 
 	if (do_tidy_mem)
 		release_all_vars();
-
-	/* keep valgrind happier */
-	if (extra_stack)
-		efree(extra_stack);
 
 	final_exit(exit_val);
 	return exit_val;	/* to suppress warnings */
