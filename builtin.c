a0cb4186e1196bab1b8d261d944d75740e7bd5a2
Arnold D. Robbins <arnold@skeeve.com>
2025-09-01 17:21:27 +0300

    Move test program for add_thousands() from builtin.c to printf.c.

--- builtin.c
+++ builtin.c
@@ -3398,25 +3398,3 @@ gawk_system(const char *command)
 	}
 #endif /* ! (defined(VMS) || defined(__MINGW32__)) */
 }
-
-#if 0
-// test program
-
-int main(int argc, char **argv)
-{
-	struct lconv *l;
-
-	setlocale(LC_ALL, "");
-	l = localeconv();
-
-	const char *new = add_thousands("12345678901234567890.54321", l);
-	printf("%s\n", new);
-	free((void*) new);
-
-	new = add_thousands("12345678901234567890", l);
-	printf("%s\n", new);
-	free((void*) new);
-
-	return 0;
-}
-#endif
