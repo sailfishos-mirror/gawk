d994ddbd5a11d640a0a4ddfb2567fce8c66f5c13
Arnold D. Robbins <arnold@skeeve.com>
2025-12-27 19:18:06 +0200

    Fix after make distcheck.

--- pc/config.h
+++ pc/config.h
@@ -322,7 +322,7 @@
 #define PACKAGE_NAME "GNU Awk"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "GNU Awk 5.3.61"
+#define PACKAGE_STRING "GNU Awk 5.3.63"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "gawk"
@@ -331,7 +331,7 @@
 #define PACKAGE_URL "http://www.gnu.org/software/gawk/"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "5.3.61"
+#define PACKAGE_VERSION "5.3.63"
 
 /* Define to 1 if *printf supports %a format */
 #define PRINTF_HAS_A_FORMAT 1
@@ -459,7 +459,7 @@
 
 
 /* Version number of package */
-#define VERSION "5.3.61"
+#define VERSION "5.3.63"
 
 /* Number of bits in a file offset, on hosts where this is settable. */
 #undef _FILE_OFFSET_BITS
