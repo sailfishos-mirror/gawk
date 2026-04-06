1777136b5d66be18eb5becd656362568a35f9e18
Arnold D. Robbins <arnold@skeeve.com>
2026-04-06 10:37:24 +0300

    Update minrx, fixes bug in non-UTF and non-8-bit domains.

--- support/minrx.c
+++ support/minrx.c
@@ -1080,11 +1080,9 @@ cset_parse(Compile *c, CSet *cs, minrx_regcomp_flags_t flags, WConv_Encoding enc
 			cset_set_range(c, cs, wclo, wchi);
 			if ((flags & MINRX_REG_ICASE) != 0) {
 				for (WChar wc = wclo; wc <= wchi; ++wc) {
-					WChar l, u;
-
-					l = enc == Byte ? tolower(wc) : towlower(wc);
-					u = enc == Byte ? toupper(wc) : towupper(wc);
-					// only add the char if it's outside the original range
+					// add case alternatives only if outside the original range
+					WChar l = enc == Byte ? tolower(wc) : towlower(wc);
+					WChar u = enc == Byte ? toupper(wc) : towupper(wc);
 					if (l < wclo || l > wchi)
 						cset_set(c, cs, l);
 					if (u < wclo || u > wchi)
@@ -1908,8 +1906,7 @@ firstbytes(Compile *c, FirstBytes *fb, int32_t *fu, WConv_Encoding e, const CSet
 {
 	if (!firstcset)
 		return false;
-	cset_firstbytes(c, firstcset, fb, fu, e);
-	return true;
+	return cset_firstbytes(c, firstcset, fb, fu, e);
 }
 
 static Regexp *
